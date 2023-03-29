6858b08fc301aa663e5f4f88d4fc74c81f628af2
Никита <drwild56@gmail.com>
2023-03-30 03:31:20 +0500

    Success  edited to IsSuccess

--- RBTB_ServiceAccount/RBTB_ServiceAccount.Application/Domains/Responses/BaseResponse.cs
+++ RBTB_ServiceAccount/RBTB_ServiceAccount.Application/Domains/Responses/BaseResponse.cs
@@ -2,7 +2,7 @@ namespace RBTB_ServiceAccount.Application.Domains.Responses;
 
 public class BaseResponse<T>
 {
-    public bool Success { get; set; } = true;
+    public bool IsSuccess { get; set; } = true;
 
     public string ErrorMessage { get; set; } = string.Empty;
     
