e4d0b573c2507f6872917920d8616539f9cbcd8d
Никита <drwild56@gmail.com>
2023-04-18 05:58:27 +0500

    create GetPositionsBySymbolRequest

---
+++ RBTB_ServiceAccount/RBTB_ServiceAccount.Application/Domains/Requests/GetPositionsBySymbolRequest.cs
@@ -0,0 +1,10 @@
+﻿using RBTB_ServiceAccount.Application.Domains.Entities.Enums;
+
+namespace RBTB_ServiceAccount.Application.Domains.Entities;
+
+public class GetPositionsBySymbolRequest
+{
+    public Side? Side { get; set; }
+    public PositionStatus? PositionStatus { get; set; }
+
+}
